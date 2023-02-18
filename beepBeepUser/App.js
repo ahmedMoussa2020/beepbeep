df314115f51f75b6182dbc0d7dc28e2f234a878f
Ahmed <>
2023-02-18 17:21:04 +0400

    rendering data from dumi data

--- beepBeepUser/App.js
+++ beepBeepUser/App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import restaurants from "./assets/data/restaurants.json";
 import RestaurantItem from "./src/components/RestaurantItem";
-import restaurants from './assets/data/restaurants.json'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RestaurantItem restaurant={restaurants[0]} />
+      {/* <RestaurantItem restaurant={restaurants[0]} />
       <RestaurantItem restaurant={restaurants[1]} />
       <RestaurantItem restaurant={restaurants[2]} />
-      <RestaurantItem restaurant={restaurants[3]} />
+      <RestaurantItem restaurant={restaurants[3]} /> */}
+      <FlatList
+        data={restaurants}
+        renderItem={({ item }) => <RestaurantItem restaurant={item} />}
+      />
 
       <StatusBar style="auto" />
     </View>
