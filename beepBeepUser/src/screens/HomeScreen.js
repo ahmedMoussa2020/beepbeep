7035b246167398fe2bea999f560117ce01dc9224
Ahmed <>
2023-02-21 18:22:19 +0400

    restaurant details screen

--- beepBeepUser/src/screens/HomeScreen.js
+++ beepBeepUser/src/screens/HomeScreen.js
@@ -1,15 +1,21 @@
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import restaurants from "../../assets/data/restaurants.json";
-import RestaurantItem from "../components/RestaurantItem";
+import RestaurantItem from "../components/RestaurantItem.js";
 
 export default function HomeScreen() {
   return (
-    <FlatList
-      data={restaurants}
-      renderItem={({ item }) => <RestaurantItem restaurant={item} />}
-      showsVerticalScrollIndicator={false}
-    />
+    <View style={styles.page}>
+      <FlatList
+        data={restaurants}
+        renderItem={({ item }) => <RestaurantItem restaurant={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    padding: 10,
+  },
+});
