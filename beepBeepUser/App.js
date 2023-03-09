0a92f7f3f5ea71d91bf10b1bf5bcf28eda936a9b
Ahmed <>
2023-03-09 15:18:47 +0300

    install aws amplify set up dependencies

--- beepBeepUser/App.js
+++ beepBeepUser/App.js
@@ -3,6 +3,11 @@ import RootNavigator from "./src/navigation";
 
 import { NavigationContainer } from "@react-navigation/native";
 
+import { Amplify } from "aws-amplify";
+import config from "./src/aws-exports";
+
+Amplify.configure(config);
+
 export default function App() {
   return (
     <NavigationContainer>
